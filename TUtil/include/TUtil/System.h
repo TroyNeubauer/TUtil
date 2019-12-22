8068d31f6407ed96325b7e3bc3b082b258287c72
Troy Neubauer <troyneubauer@gmail.com>
2019-12-22 12:45:53 -0500

    Removed redudant functions and fixed missing windows header

--- TUtil/include/TUtil/System.h
+++ TUtil/include/TUtil/System.h
@@ -3,6 +3,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifdef T_PLATFORM_WINDOWS
+	#include <malloc.h>
+#endif
+
 namespace TUtil {
 
 	class System
@@ -41,13 +45,6 @@ namespace TUtil {
 		static uint64_t PageSize();
 		static uint64_t AllocationGranularity();
 		static uint64_t PerformanceCounterResulution();
-
-		static uint64_t LogFloor(uint64_t value);
-		//Computes the log base 2 for the value and rounds up. Ex 2->1, 4->2, 5->3, 8->3
-		//We need to make powers of two produce the largest value for a given input.
-		//because LogFloor(5)->2, LogFloor(4) is still 2 and with a +1 we get the right anwser
-		//LogFloor(4)->2, Log2Celi(3)->1+1->2
-		static inline uint64_t Log2Celi(uint64_t value) { return LogFloor(value - 1) + 1; }
 	};
 
 	//Definitions to template functions
